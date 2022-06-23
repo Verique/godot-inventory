4058fe18b9e6d3bbb524e84bd67c36686f595e45
verique <>
2022-06-23 18:46:20 +0300

    cleanup + some test nodes

---
+++ Scripts/Models/Inventory/InventoryItem.cs
@@ -0,0 +1,5 @@
+namespace Grate.Models.Inventory{
+    public class InventoryItem{
+        public string Name {get; set;}
+    }
+}
---
+++ Scripts/Services/Inventory/InventoryService.cs
@@ -0,0 +1,14 @@
+using Grate.Models.Inventory;
+
+namespace Grate.Services
+{
+    public interface IInventoryService : IService { }
+
+    public class InventoryService : IInventoryService
+    {
+        ItemsContainer items;
+
+        public void A(){
+        }
+    }
+}
